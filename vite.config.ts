b8144d2e8d7ca3a31ec1a08f91443bfdcc117364
Valentina <vliberonazuniga@gmail.com>
2025-12-29 16:09:07 -0300

    feat: updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SimpleProtafolio/', // Update this to match your GitHub repo name
+  base: '/ValentinaLZProtafolio/',
 })
